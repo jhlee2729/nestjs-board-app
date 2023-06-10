ba50baa4f896032e94c5434efa1b6ed77bcf689b
jhlee2729 <wkdgml7644@gmail.com>
2023-06-10 22:07:42 +0900

    게시물 생성 - Controller 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,5 +9,13 @@ export class BoardsController {
     @Get('/')
     getAllBoard(): Board[] { //핸들러
         return this.boardsService.getAllBoards(); //서비스에서 요청을 처리
+    }
+
+    @Post()
+    createBoard(
+        @Body('title') title:string,
+        @Body('description') description:string
+    ): Board {
+        return this.boardsService.createBoard(title, description)
     }
 }
