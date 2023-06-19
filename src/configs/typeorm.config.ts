1dbe98537fb23f85bb726a7611ba3fe75eea855e
jhlee2729 <wkdgml7644@gmail.com>
2023-06-19 21:36:36 +0900

    port 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeORMConfig : TypeOrmModuleOptions = {
     type:'postgres',
     host:'localhost',
-    port:543,
+    port:5432,
     username:'postgres',
     password:'postgres',
     database: 'board-app',
