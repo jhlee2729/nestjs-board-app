66ba0c4acb8efb9ae39326255ea01be7f22d7fbe
jhlee2729 <wkdgml7644@gmail.com>
2023-07-01 14:40:30 +0900

    로그인 기능 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -10,8 +11,20 @@ export class AuthService {
         private userRepository: UserRepository,
     ) {}
 
-    async signUp(authCredentialsDto: AuthCredentialsDto) :Promise<void> {
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.createUser(authCredentialsDto);
 
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        
+        const { username, password } = authCredentialsDto;
+        const user = await this.userRepository.findOne({ username });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            return 'logIn success';
+        } else {
+            throw new UnauthorizedException('logIn failed');
+        }
     }
 }
