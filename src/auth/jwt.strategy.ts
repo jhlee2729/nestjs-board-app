69d535e3ba89e985086ffc18703947b966abcb93
jhlee2729 <wkdgml7644@gmail.com>
2023-07-01 21:51:45 +0900

    설정과 환경 변수 코드에 적용

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         private userRepository: UserRepository
     ){
         super({
-            secretOrKey: 'Secret1234',
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
         })
     }
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbCondif = config.get('db');
 
 export const typeORMConfig : TypeOrmModuleOptions = {
-    type:'postgres',
-    host:'localhost',
-    port:5432,
-    username:'postgres',
-    password:'postgres',
-    database: 'board-app',
+    type:dbCondif.type,
+    host:process.env.RDS_HOSTNAME || dbCondif.host,
+    port: process.env.RDS_PORT || dbCondif.port,
+    username: process.env.RDS_USERNAME || dbCondif.username,
+    password:process.env.RDS_PASSWORD ||
+    dbCondif.passowrd,
+    database: process.env.RDS_DB_NAME || dbCondif.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize:true
+    synchronize: dbCondif.synchronize
 }
